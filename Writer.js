e685b92975693c0ac02758ff0dce7d7a8d7e98b1
VoTanTruong <truong@world-works.co.jp>
2021-03-30 11:00:15 +0900

    edit folder log path

--- Writer.js
+++ Writer.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 module.exports = class Writer {
 
-	constructor (collector) {
+	constructor(collector) {
 		this.collector = collector;
 		this.day = null;
 		this.fileStream = null;
@@ -15,37 +15,37 @@ module.exports = class Writer {
 	/**
 	 * ファイル名にcollectorが集めたデータを書き出す。
 	 */
-	write (date) {
-		const line = this.collector.getData (date);
-		this.writeData (date,line);
+	write(date) {
+		const line = this.collector.getData(date);
+		this.writeData(date, line);
 	}
 
 	/**
 	 * ファイル名に１行分のデータを書き出す
 	 */
-	writeData (date,line) {
-		const fileStream = this.getFileStream (date);
-		fileStream.write (line);
+	writeData(date, line) {
+		const fileStream = this.getFileStream(date);
+		fileStream.write(line);
 	}
 
 	/**
 	 * 日付からFileStreamを返します。
 	 */
-	getFileStream (date) {
+	getFileStream(date) {
 		const newDay = date.getDate();
 		if (this.day === newDay) return this.fileStream;
 		if (this.fileStream) this.fileStream.end('');  // day change close stream
 		this.day = newDay;
-		this.fileName = this.newFileName (date,newDay);
-		this.fileStream = fs.createWriteStream(this.fileName, {flags: 'a'});
+		this.fileName = this.newFileName(date, newDay);
+		this.fileStream = fs.createWriteStream(this.fileName, { flags: 'a' });
 		return this.fileStream;
 	}
 
 	/**
 	 * 日付からファイル名を返します。
 	 */
-	newFileName (date,day) {
-		return 'log_' + date.getFullYear() + '_' + date.getMonth()+1 + '_' + day + '.csv';
+	newFileName(date, day) {
+		return 'log/log_' + date.getFullYear() + '_' + date.getMonth() + 1 + '_' + day + '.csv';
 	}
 
 }
